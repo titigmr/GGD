4e903dffea021ade8fa2e0e94af83a502e7020da
titigmr <thierry.gmr@outlook.fr>
2022-01-07 04:40:43 +0100

    change setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
 
-REQUIRED_PACKAGES = open('requirements.txt').readlines()
+REQUIRED_PACKAGES = [
+    'tqdm==4.62.3',
+    'requests==2.27.1',
+    'selenium==4.1.0',
+]
 
 setup(
     name="ggdl",
+    version='0.0.1',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     description='Google image downloader'
