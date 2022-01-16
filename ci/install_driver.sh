b7ed64296b309cb76981fb1adb44e0c145b83367
titigmr <thierry.gmr@outlook.fr>
2022-01-16 20:59:26 +0100

    add condtion

--- ci/install_driver.sh
+++ ci/install_driver.sh
@@ -2,16 +2,14 @@
 
 if [ $* = "firefox" ];
     then
-    wget https://github.com/mozilla/geckodriver/\
-    releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
+    wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
     tar -zxvf geckodriver-v0.30.0-linux64.tar.gz
     rm geckodriver-v0.30.0-linux64.tar.gz
     mv geckodriver /usr/local/bin/
 
 elif [ $* = 'chrome' ]
     then
-    wget https://chromedriver.storage.googleapis.com/98.0.4758.48/chromedriver\
-    _linux64.zip
+    wget https://chromedriver.storage.googleapis.com/98.0.4758.48/chromedriver_linux64.zip
     tar -zxvf chromedriver_linux64.zip
     rm chromedriver_linux64.zip
     mv chromedriver /usr/local/bin/
